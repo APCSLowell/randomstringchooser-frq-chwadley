0e6fa2e750808604acb66f821c8402444077eb57
chwadley <chwadley@s.sfusd.edu>
2025-04-24 11:13:25 -0700

    a

--- src/main/java/RandomStringChooser.java
+++ src/main/java/RandomStringChooser.java
@@ -1,8 +1,17 @@
 import java.util.*;
-public class RandomStringChooser
-{
-  /* to be implemented in part (a) */
-  
-  //Heads up! 
-  //You will get a very confusing error message until you have working code in part b as well
+public class RandomStringChooser {
+    private ArrayList<String> a;
+    
+    public RandomStringChooser(String[] _a) {
+        a = new ArrayList<String>();
+        for (int i=0;i<_a.length;i++) {
+            a.add(_a[i]);
+        }
+    }
+
+    public String getNext() {
+        if (a.size()==0) return new String("NONE");
+        int i= (int)(Math.random()*a.size());
+        return a.remove(i);
+    }
 }
